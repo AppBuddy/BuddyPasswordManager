3cf0d76c4f9e21c9d7003e144e47f8096954ee4a
Christopher Bell <christopherbell1813@gmail.com>
2019-02-11 22:56:39 -0600

    This wil now call the gui class to create the main dialog

--- src/main/java/com/appbuddy/buddypasswordmanager/App.java
+++ src/main/java/com/appbuddy/buddypasswordmanager/App.java
@@ -1,5 +1,7 @@
 package com.appbuddy.buddypasswordmanager;
 
+import com.appbuddy.buddypasswordmanager.gui.GUI;
+
 /**
  * Hello world!
  *
@@ -7,5 +9,7 @@ package com.appbuddy.buddypasswordmanager;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(System.getProperty("user.name"));
+        GUI gui = new GUI();
     }
 }
