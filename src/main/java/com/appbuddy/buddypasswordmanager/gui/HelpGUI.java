66c32dc9055d287cef5468d00668f512d02b3821
Christopher Bell <christopherbell1813@gmail.com>
2019-04-18 20:59:32 -0500

    Changed spacing to the superior 4 spaces instead of tabs

--- src/main/java/com/appbuddy/buddypasswordmanager/gui/HelpGUI.java
+++ src/main/java/com/appbuddy/buddypasswordmanager/gui/HelpGUI.java
@@ -19,51 +19,51 @@ import javax.swing.JTextArea;
  */
 
 public class HelpGUI {
-	private static final Logger LOGGER = Logger.getLogger(HelpGUI.class.getName());
-	private JFrame frame;
-	private JPanel panel;
-	private JLabel label;
-	private JTextArea textArea;
-	private JScrollPane scroll;
-	private JButton button;
+    private static final Logger LOGGER = Logger.getLogger(HelpGUI.class.getName());
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
+    private JTextArea textArea;
+    private JScrollPane scroll;
+    private JButton button;
 
-	public HelpGUI() {
-		this.frame = new JFrame("Help");
-		this.label = new JLabel("About this Program");
-		this.textArea = new JTextArea(5, 20);
-		this.scroll = new JScrollPane(textArea);
-		this.button = new JButton("Button");
-		this.initFrame();
-		this.initTextArea();
-		this.frame.add(scroll);
-		this.scroll.setBounds(0, 0, 350, 370);
-		this.button.addActionListener(new Action());
-	}
+    public HelpGUI() {
+        this.frame = new JFrame("Help");
+        this.label = new JLabel("About this Program");
+        this.textArea = new JTextArea(5, 20);
+        this.scroll = new JScrollPane(textArea);
+        this.button = new JButton("Button");
+        this.initFrame();
+        this.initTextArea();
+        this.frame.add(scroll);
+        this.scroll.setBounds(0, 0, 350, 370);
+        this.button.addActionListener(new Action());
+    }
 
-	private void initFrame() {
-		this.frame.setSize(350, 370);
-		this.frame.setVisible(true);
-		this.frame.getContentPane();
-	}
+    private void initFrame() {
+        this.frame.setSize(350, 370);
+        this.frame.setVisible(true);
+        this.frame.getContentPane();
+    }
 
-	private void initTextArea() {
-		this.textArea.setText("Help!");
-		this.textArea.setFont(new Font("Comic Sans", Font.BOLD, 20));
-		this.textArea.setWrapStyleWord(true);
-		this.textArea.setLineWrap(true);
-		this.textArea.setEditable(false);
-	}
+    private void initTextArea() {
+        this.textArea.setText("Help!");
+        this.textArea.setFont(new Font("Comic Sans", Font.BOLD, 20));
+        this.textArea.setWrapStyleWord(true);
+        this.textArea.setLineWrap(true);
+        this.textArea.setEditable(false);
+    }
 
-	static class Action implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			LOGGER.log(Level.INFO, "Help Button Clicked");
-			JFrame frame2 = new JFrame("Clicked!");
-			frame2.setVisible(true);
-			frame2.setSize(200, 200);
-			JLabel label2 = new JLabel("Button has been clicked");
-			JPanel panel2 = new JPanel();
-			frame2.add(panel2);
-			panel2.add(label2);
-		}
-	}
+    static class Action implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            LOGGER.log(Level.INFO, "Help Button Clicked");
+            JFrame frame2 = new JFrame("Clicked!");
+            frame2.setVisible(true);
+            frame2.setSize(200, 200);
+            JLabel label2 = new JLabel("Button has been clicked");
+            JPanel panel2 = new JPanel();
+            frame2.add(panel2);
+            panel2.add(label2);
+        }
+    }
 }
