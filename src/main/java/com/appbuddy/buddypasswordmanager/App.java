6814aa57f1d68809c28d312542d6070e0fc6ef3c
Timothy Strimple <tim@tstrimple.com>
2021-05-10 09:20:53 -0500

    This project has officially moved beyond the Hello World stage

--- src/main/java/com/appbuddy/buddypasswordmanager/App.java
+++ src/main/java/com/appbuddy/buddypasswordmanager/App.java
@@ -8,7 +8,6 @@ import com.appbuddy.buddypasswordmanager.gui.MainGUI;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println(System.getProperty("user.name"));
         MainGUI gui = new MainGUI();
     }
